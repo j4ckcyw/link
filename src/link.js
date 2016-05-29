82b0c9355dcf315494fa4cc1da838086fdda45eb
Jack Chan <chanjack@amazon.com>
2016-05-28 23:46:09 -0700

    Create overlap and open link

--- src/link.js
+++ src/link.js
@@ -29,10 +29,26 @@ mousedrags.subscribe(e => {
   el.style.height = e.height + "px"
 })
 
+let overlaps = (el1, el2) => {
+  let rect1 = el1.getBoundingClientRect()
+    , rect2 = el2.getBoundingClientRect()
+    , left = Math.max(rect1.left, rect2.left)
+    , right = Math.min(rect1.right, rect2.right)
+    , top = Math.max(rect1.top, rect2.top)
+    , bottom = Math.min(rect1.bottom, rect2.bottom)
+
+    return left < right && top < bottom
+}
+
 // remove rectangle
 mouseups.subscribe(_ => {
   let el = document.querySelector("#rect")
   if (!el) return
+
+  let as = [...document.querySelectorAll('a')]
+    .filter(a => overlaps(el, a))
+    .map(a => window.open(a))
+    .map(a => console.log(a))
 
   el.remove()
 })
