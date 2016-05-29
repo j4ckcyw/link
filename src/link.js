8e43c87a875443f0a0a610815f816f6fc48ae7b0
Jack Chan <chanjack@amazon.com>
2016-05-28 22:05:39 -0700

    Create, move, and destroy rectangle

--- src/link.js
+++ src/link.js
@@ -1,4 +1,4 @@
-let Rx = require('rxjs/Rx');
+let Rx = require('rxjs/Rx')
 
 let mousedowns = Rx.Observable.fromEvent(document, 'mousedown')
   , mousemoves = Rx.Observable.fromEvent(document, 'mousemove')
@@ -7,10 +7,34 @@ let mousedowns = Rx.Observable.fromEvent(document, 'mousedown')
 let mousedrags = mousedowns.flatMap(x => mousemoves.map(y => ({
   left: x.clientX,
   top: x.clientY,
-  right: y.clientX,
-  bottom: y.clientY
-})).takeUntil(mouseups));
+  width: y.clientX - x.clientX,
+  height: y.clientY - x.clientY
+})).takeUntil(mouseups))
 
-mousedrags.subscribe(x => console.log('mousedrags' + x))
+// create and append rectangle
+mousedowns.subscribe(_ => {
+  let el = document.createElement('div')
+  el.id = "rect"
+  el.style.position = "absolute"
+  el.style.border = "1px dotted lightblue"
+  document.body.appendChild(el)
+})
+
+// move rectangle
+mousedrags.subscribe(e => {
+  let el = document.querySelector("#rect")
+  el.style.left = e.left + "px"
+  el.style.top = e.top + "px"
+  el.style.width = e.width + "px"
+  el.style.height = e.height + "px"
+})
+
+// remove rectangle
+mouseups.subscribe(_ => {
+  let el = document.querySelector("#rect")
+  if (!el) return
+
+  el.remove()
+})
 
 console.log("loaded");
